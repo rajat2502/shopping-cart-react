d8d3f7eba55f720c0262691c9100b1682cd2a635
Rajat Verma <rajatverma5885045@gmail.com>
2019-11-08 10:04:41 +0530

    added sidebar

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,21 +1,37 @@
-import React, {Fragment} from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 import Card from './Card';
 
 const CardList = ({products}) => {
+
+    const [delay, setDelay] = useState(true)
+
+    useEffect(() => {
+        setDelay(true);
+        setTimeout(() => {
+            setDelay(false)
+        }, 1000);
+    }, [products])
+
     return (
         <Fragment>
-            <span className="products-length">{products.length} Product(s) found.</span>
-            <div className="card-list">
-                {
-                    products.length == 0 ? <h3 className="text-center">Loading Data...</h3> :
-                    products.map(item => {
-                        return (
-                            <Card key={item.id} data={item} />
-                        )
-                    })
-                }
-            </div>
+            {
+                delay ? <img src="https://career.alliedvision.com/persis/images_avt/gicccccphy.gif" alt="loader" className="loader" />
+                :
+                <div>
+                    <span className="products-length">{products.length} Product(s) found.</span>
+                    <div className="card-list">
+                        {
+                            products.length === 0 ? <p className="text-center">Sorry, No products of the specified categories :-(</p> :
+                            products.map(item => {
+                                return (
+                                    <Card key={item.id} data={item} />
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            }
         </Fragment>
     )
 }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Cart = () => {
+
+    const [classActive, toggleClass] = useState(false);
+
+    const toggleButton = () => {
+        toggleClass(!classActive);
+    }
+
     return (
-        <div className="cart">
-            Cart
-        </div>
+        <Fragment>
+            <div id="sidebar" className={classActive ? "active" : ""}>
+            <div className="toggle-btn" onClick={toggleButton}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            </div>
+        </Fragment>
     )
 }
 
